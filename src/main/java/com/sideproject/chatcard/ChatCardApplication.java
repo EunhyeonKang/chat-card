ca9824be5665816ff21e31cad21d9837d4b495cc
EunhyeonKang <rkddmsgus1@daum.net>
2022-11-23 15:23:48 +0900

    first commit

---
+++ src/main/java/com/sideproject/chatcard/ChatCardApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.chatcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatCardApplication.class, args);
+	}
+
+}
